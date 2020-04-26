6dfe1154a63e2bb422be9887730aefb8f2cf37e7
= <gonzalo.ricco1@gmail.com>
2020-04-26 20:00:50 -0300

    Put binding.pry

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -2,6 +2,7 @@ class JsonWebToken
   HMAC_SECRET = Rails.application.secrets.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
+    binding.pry
     payload[:exp] = exp.to_i
     JWT.encode(payload, HMAC_SECRET)
   end
